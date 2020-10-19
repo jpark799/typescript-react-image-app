35e978896ffa26d8e22a5f10ed1984d84592cb17
jpark799 <jason.j.park79@gmail.com>
2020-10-19 00:37:01 -0400

    linter

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from "react";
 import "./styles.css";
-import {Header} from "./components"
+import { Header } from "./components";
 
 export default function App() {
 	interface IImage {
@@ -28,32 +28,40 @@ export default function App() {
 			.then((response) => response.json())
 			.then((data: IImage[]) => {
 				data.sort((a: IImage, b: IImage) => a.author.localeCompare(b.author));
-				data.map((data) => {data.height= 133; data.width=200})
+				data.map((data) => {
+					data.height = 133;
+					data.width = 200;
+				});
 				setImages(data);
 				setLoadComplete(true);
 			});
 	}, []);
 
-	const	resize = (number: number) => {
-    const img = document.getElementsByTagName('img')[number]; 
-    img.style.height = '200px'; 
-    img.style.width = '300px';
-}
-
 	return (
-    <div className="App">
-			<Header/>
-      {!loadComplete ? (
-        <div className="loader" />
-      ) : (
-        <div>
-          {images.map((image) => (
-            <div className="image-row" key={image.id}>
-              <img className="image" width={image.width} height={image.height} src={image.download_url} alt={image.author} onClick={() => {image.height=200; image.width=300; setImages([...images])}} />
-            </div>
-          ))}
-        </div>
-      )}
-    </div>
-  );
+		<div className="App">
+			<Header />
+			{!loadComplete ? (
+				<div className="loader" />
+			) : (
+				<div>
+					{images.map((image) => (
+						<div className="image-row" key={image.id}>
+							<img
+								className="image"
+								width={image.width}
+								height={image.height}
+								src={image.download_url}
+								alt={image.author}
+								onClick={() => {
+									image.height = 200;
+									image.width = 300;
+									setImages([...images]);
+								}}
+							/>
+						</div>
+					))}
+				</div>
+			)}
+		</div>
+	);
 }
