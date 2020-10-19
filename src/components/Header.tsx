37d170e5d0999ab20912f6b07415dbdd212c81d1
jpark799 <jason.j.park79@gmail.com>
2020-10-19 00:12:54 -0400

    upodated header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,13 +1,12 @@
 import "../styles.css";
-import React from 'react'
-
+import React from "react";
 
 export const Header = () => {
-  return (
-    <div className="header">
-      <h1>
-        📸 Conductiv Coding Challenge - <a href="https://www.linkedin.com/in/jason-park-0790aab6/"> Jason Park  </a>
-      </h1>
-    </div>
-  )
-}
+	return (
+		<div className="header">
+			<h1>
+					<span role="img" aria-label="camera"> 📸 </span> Conductiv Coding Challenge - <a href="https://www.linkedin.com/in/jason-park-0790aab6/">	Jason Park</a>
+			</h1>
+		</div>
+	);
+};
